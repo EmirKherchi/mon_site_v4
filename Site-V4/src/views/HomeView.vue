584f4f2773c23eccc1ee346b5e6f50fb722099fe
Emir Kherchi <contact@ekherchi.fr>
2024-01-17 15:06:31 +0100

    init vue 3

---
+++ Site-V4/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+const test = ref("test");
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+
+    {{ test }}
+  </main>
+</template>
