925d8509c5b0c72ce69329d0b0ce69b97b9c70ac
Emir Kherchi <contact@ekherchi.fr>
2024-01-18 09:15:31 +0100

    get data tests

--- Site-V4/src/views/HomeView.vue
+++ Site-V4/src/views/HomeView.vue
@@ -1,13 +1,32 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
-import { ref } from 'vue'
-const test = ref("test");
+import { onMounted } from 'vue'
+
+const API_URL = `http://admin.ekherchi.fr/wp-json/wp/v2/projects?acf_format=standard&_fields=acf`
+let projects = null;
+
+const getData = () => {
+  fetch(API_URL, {
+    headers: { 'Content-type': 'application/json' },
+  }).then(res=>res.json()).then((response) => {
+    // console.log(response)
+    projects = response;
+    console.log(projects[0])
+  }).catch((error) => {
+    console.log('Looks like there was a problem: \n', error);
+  });
+}
+
+
+onMounted(() => {
+  getData();
+
+})
 </script>
 
 <template>
   <main>
     <TheWelcome />
-
-    {{ test }}
+    {{ projects[0].acf.project.name}}
   </main>
 </template>
