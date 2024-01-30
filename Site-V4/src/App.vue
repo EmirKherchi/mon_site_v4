b33e759fffdb1c1aff6bb9efba7acfd5c3a566b0
Emir <contact@ekherchi.fr>
2024-01-30 09:59:04 +0100

    Add package Font-Awesome

--- Site-V4/src/App.vue
+++ Site-V4/src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-// import { RouterLink, RouterView } from 'vue-router'
 import NavHeader from './components/NavHeader.vue'
 import NavFooter from './components/NavFooter.vue'
 </script>
