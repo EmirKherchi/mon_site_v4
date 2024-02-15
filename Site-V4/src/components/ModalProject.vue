75298b235a3b898836c4f884cb1e7e2e92082c17
Emir <contact@ekherchi.fr>
2024-02-15 16:27:00 +0100

    Modal project with static data

--- Site-V4/src/components/ModalProject.vue
+++ Site-V4/src/components/ModalProject.vue
@@ -3,34 +3,111 @@
     id="modal">
     <template #header>
       <div class="flex items-center text-lg">
-        Terms of Service
+        <fwb-heading tag="h2" class="text-center mt-3 lg:text-left lg:mt-0 text-primary tracking-wide">
+          {{ props.project?.title || 'Présentation du projet' }}
+        </fwb-heading>
       </div>
     </template>
     <template #body>
-      <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-        With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
-        companies around the world are updating their terms of service agreements to comply.
-      </p>
-      <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-        The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to
-        ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as
-        possible of high-risk data breaches that could personally affect them.
-      </p>
+      <div class="flex flex-col-reverse lg:flex-row gap-6 items-center justify-center mx-auto p-3 lg:min-h-[425px]">
+        <div class="h-full flex items-center justify-center w-full">
+          <fwb-accordion flush class="w-full lg:min-w-[505px] ">
+            <fwb-accordion-panel>
+              <fwb-accordion-header>
+                <span class="text-secondary">
+                  Le Projet
+                </span>
+              </fwb-accordion-header>
+              <fwb-accordion-content>
+                <div>
+                  <p class="mb-2 text-secondary-light font-light dark:text-gray-400">
+                    Flowbite is an open-source library of interactive components built on top of Tailwind CSS including
+                    buttons, dropdowns, modals, navbars, and more.
+                  </p>
+                  <p class="text-secondary-light font-light dark:text-gray-400">
+                    Check out this guide to learn how to <a class="text-blue-600 dark:text-blue-500 hover:underline"
+                      href="/docs/getting-started/introduction/">get started</a> and start developing websites even faster
+                    with components on top of Tailwind CSS.
+                  </p>
+                </div>
+              </fwb-accordion-content>
+            </fwb-accordion-panel>
+            <fwb-accordion-panel>
+              <fwb-accordion-header>
+                <span class="text-secondary">
+                  Compétences mobilisées
+                </span>
+              </fwb-accordion-header>
+              <fwb-accordion-content>
+                <div>
+                  <p class="mb-2 text-secondary-light font-light dark:text-gray-400">
+                    Flowbite is first conceptualized and designed using the Figma software so everything you see in the
+                    library has a design equivalent in our Figma file.
+                  </p>
+                  <p class="text-secondary-light font-light dark:text-gray-400">
+                    Check out the <a class="text-blue-600 dark:text-blue-500 hover:underline"
+                      href="https://flowbite.com/figma/">Figma design system</a> based on the utility classes from
+                    Tailwind CSS and components from Flowbite.
+                  </p>
+                </div>
+              </fwb-accordion-content>
+            </fwb-accordion-panel>
+            <fwb-accordion-panel>
+              <fwb-accordion-header>
+                <span class="text-secondary">
+                  Choix technique / Stack
+                </span>
+              </fwb-accordion-header>
+              <fwb-accordion-content>
+                <div>
+                  <p class="mb-2 text-secondary-light font-light dark:text-gray-400">
+                    Flowbite is first conceptualized and designed using the Figma software so everything you see in the
+                    library has a design equivalent in our Figma file.
+                  </p>
+                  <p class="text-secondary-light font-light dark:text-gray-400">
+                    Check out the <a class="text-blue-600 dark:text-blue-500 hover:underline"
+                      href="https://flowbite.com/figma/">Figma design system</a> based on the utility classes from
+                    Tailwind CSS and components from Flowbite.
+                  </p>
+                </div>
+              </fwb-accordion-content>
+            </fwb-accordion-panel>
+          </fwb-accordion>
+        </div>
+        <div class="flex flex-col gap-6 items-center justify-center w-full">
+          <fwb-img alt="flowbite-vue" size="lg:max-w-lg" img-class="rounded-lg"
+            src="http://admin.ekherchi.fr/wp-content/uploads/2024/01/pexels-felix-mittermeier-956981-scaled.jpg" />
+          <Buttons color="accent" class="group">
+            Voir Le Site
+            <font-awesome-icon icon="fa-solid fa-arrow-right"
+              class="ms-1 group-hover:translate-x-[5px] transtion-all duration-300" />
+          </Buttons>
+        </div>
+      </div>
     </template>
   </fwb-modal>
 </template>
 
 <script setup>
-import { FwbModal, FwbHeading } from 'flowbite-vue'
-
+import {
+  FwbModal,
+  FwbHeading,
+  FwbImg,
+  FwbAccordion,
+  FwbAccordionContent,
+  FwbAccordionHeader,
+  FwbAccordionPanel,
+} from 'flowbite-vue'
+import Buttons from '../components/Buttons/Buttons.vue'
 const props = defineProps({
   showModal: {
     type: Boolean,
     default: false,
+  },
+  project: {
+    type: Object,
   }
 })
-
-
 </script>
 
 <style>
